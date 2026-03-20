62dc1d798ef4b8b28be2ed166d3e420f1ff11eea
jaredmarin18-ship-it <jaredmarin18@gmail.com>
2026-03-20 16:31:13 -0600

    se agrega un documento de python

---
+++ Ejemplo1.py
@@ -0,0 +1,25 @@
+# es un comentario
+nombre = "Jared"
+print(nombre)
+
+edad = 40
+
+print(nombre, edad, "years old")  # concatenar
+
+texto_largo = """
+la vida, en su forma más simple, es un regalo. Disfrutar del café por la mañana, la sonrisa de un extraño, el olor a tierra mojada, o simplemente la paz de saber que estás aquí, respirando. Recuerda que 
+"""
+print(texto_largo)
+
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("el numero de la suma es:", resultado)
+resultado = numero2 / numero1
+print("el numero de la division es:", resultado)
+resultado = numero1 - numero2
+print("el numero de la resta es:", resultado)
+resultado = numero2 * numero1
+print("el numero de la multiplicacion es:", resultado)
