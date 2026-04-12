f4c2e10117d1e59f5f6fa245876463a7900234dc
jaredmarin18-ship-it <jaredmarin18@gmail.com>
2026-04-11 21:37:39 -0600

    bloque de texto contar cuantas veces se repite la a y dividir con splitlines

---
+++ Examen_ParciaII/parcial8.py
@@ -0,0 +1,23 @@
+cancion = """Todo empezó con la mirada que cruzamo' en la disco
+Que nos vimo' aquella ve' (aquella ve')
+Yo no sé cómo me encontraste, pero, baby
+Al otro día me explotaste el DM (oh-oh-oh, el DM)
+¿Cómo llegaste a mi cama? Si juré que yo contigo, mami
+No iba a volver (no iba a volver)
+Pero siempre sigo al diablito, me levanto
+Miro al techo, y digo, "Ya volví a caer"
+Aun así te sigo amando
+Y tú buscando una aventura (una aventura)
+Yo nunca termino entendiendo
+Cómo tú me tiene' al límite de la locura (oh-oh, eh)
+Aun así te sigo amando
+Y tú buscando una aventura (una aventura)
+Yo nunca termino entendiendo
+Cómo tú me tiene' al límite de la locura (El Ozo)"""
+
+conteo = cancion.count("a")
+
+separar = cancion.splitlines()
+
+print("total de veces letra a", conteo)
+print("cadena separada", separar)
