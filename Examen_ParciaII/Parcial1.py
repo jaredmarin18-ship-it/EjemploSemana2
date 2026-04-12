fa856fef45e7d756c949ed7e9be0c4fb62f47008
jaredmarin18-ship-it <jaredmarin18@gmail.com>
2026-04-11 21:30:22 -0600

    limpiar la palabra elefante y y conteo de e

---
+++ Examen_ParciaII/Parcial1.py
@@ -0,0 +1,10 @@
+# 1
+cadena = "  elefante  "
+
+cadena_limpia = cadena.strip()
+
+conteo_e = cadena_limpia.count("e")
+
+print("Texto original:", cadena)
+print("Texto limpio:", cadena_limpia)
+print("La letra 'e' aparece:", conteo_e, "veces.")
