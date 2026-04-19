1ae35a148c66907c22087d9955399fd783ea45b2
jaredmarin18-ship-it <jaredmarin18@gmail.com>
2026-04-18 18:29:49 -0600

    se agrega el ejercicio 1

---
+++ EjerciciosWhileForIf/Ejepractica1.py
@@ -0,0 +1,8 @@
+numero = int(input("ingrese un numero: "))
+
+if numero < 0:
+    print("el numero es negativo")
+elif numero > 0 : 
+    print("el numero es positivo")
+else : 
+    print("el numero es cero")
